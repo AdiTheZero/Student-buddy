f3c21d01e0658ecf2aa4080af3e5ddf81c99e1f3
AdiTheZero <adityakumarspn555@gmail.com>
2022-06-01 01:02:57 +0530

    added PUBLIC_URL reference

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import Gri from './Gri';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <BrowserRouter>
+ <BrowserRouter basename={process.env.PUBLIC_URL}>
 <Routes>
   <Route path='/'element={<Gri></Gri>}/>
   <Route path='/news' element={<News></News>}/>
